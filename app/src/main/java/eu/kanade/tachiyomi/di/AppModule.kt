def58cc910872e7b4bcebedd9c05a5f3bcff3ff4
rianza <rianyanuar53@gmail.com>
2026-01-14 07:07:29 +0700

    add debug or log for database (appmodule)

--- app/src/main/java/eu/kanade/tachiyomi/di/AppModule.kt
+++ app/src/main/java/eu/kanade/tachiyomi/di/AppModule.kt
@@ -1,6 +1,7 @@
 package eu.kanade.tachiyomi.di
 
 import android.app.Application
+import android.util.Log
 import android.os.Build
 import androidx.core.content.ContextCompat
 import androidx.sqlite.db.SupportSQLiteDatabase
@@ -69,8 +70,8 @@ class AppModule(val app: Application) : InjektModule {
             if (securityPreferences.encryptDatabase().get()) {
                 System.loadLibrary("sqlcipher")
             }
-
             // SY <--
+
             AndroidSqliteDriver(
                 schema = Database.Schema,
                 context = app,
@@ -78,15 +79,23 @@ class AppModule(val app: Application) : InjektModule {
                 name = if (securityPreferences.encryptDatabase().get()) {
                     CbzCrypto.DATABASE_NAME
                 } else {
-                    LEGACY_DATABASE_NAME
+                    LEGACYDATABASENAME
                 },
-                factory = if (securityPreferences.encryptDatabase().get()) {
-                    SupportOpenHelperFactory(CbzCrypto.getDecryptedPasswordSql(), null, false, 25)
-                } else if (BuildConfig.DEBUG && Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-                    // Support database inspector in Android Studio
-                    FrameworkSQLiteOpenHelperFactory()
-                } else {
-                    RequerySQLiteOpenHelperFactory()
+                factory = run {
+                    val factoryUsed = when {
+                        securityPreferences.encryptDatabase().get() -> {
+                            "SupportOpenHelperFactory(SQLCipher)"
+                            SupportOpenHelperFactory(CbzCrypto.getDecryptedPasswordSql(), null, false, 25)
+                        }
+                        Build.VERSION.SDK_INT >= Build.VERSION_CODES.R -> {
+                            Log.i("DB_INIT", "Using FrameworkSQLiteOpenHelperFactory on SDK ${Build.VERSION.SDK_INT}")
+                            FrameworkSQLiteOpenHelperFactory()
+                        }
+                        else -> {
+                            Log.i("DB_INIT", "Using RequerySQLiteOpenHelperFactory on SDK ${Build.VERSION.SDK_INT}")
+                            RequerySQLiteOpenHelperFactory()
+                        }
+                    }
                 },
                 // SY <--
                 callback = object : AndroidSqliteDriver.Callback(Database.Schema) {
@@ -95,6 +104,7 @@ class AppModule(val app: Application) : InjektModule {
                         setPragma(db, "foreign_keys = ON")
                         setPragma(db, "journal_mode = WAL")
                         setPragma(db, "synchronous = NORMAL")
+                        Log.i("DB_INIT", "onOpen completed for DB name=${if (securityPreferences.encryptDatabase().get()) CbzCrypto.DATABASE_NAME else LEGACYDATABASENAME}")
                     }
                     private fun setPragma(db: SupportSQLiteDatabase, pragma: String) {
                         val cursor = db.query("PRAGMA $pragma")
